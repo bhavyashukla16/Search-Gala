0abae5923c7e117e780572472b85cb676635980c
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-05-08 02:23:32 +0530

    file upload done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
-import Upload from './upload';
+//import Upload from './upload';
 import 'semantic-ui-css/semantic.min.css'
-import { Button } from 'semantic-ui-react'
+import firebase from './firebase'
+import { Button, Modal, Header, Icon  } from 'semantic-ui-react'
+import { useState } from 'react';
 
 function App() {
+
+  const [ selectedFile, setSelectedFile ] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  const fileEventHandler = (event) => {
+    setSelectedFile(event.target.files[0])
+    };
+
+ const fileUploadHandler = () => {
+   let bucketName = 'files'
+   let storageRef = firebase.storage().ref(`${bucketName}/${selectedFile.name}`)
+   let uploadImage = storageRef.put(selectedFile)
+   uploadImage.on(firebase.storage.TaskEvent.STATE_CHANGED,
+     () => {
+       let downloadUrl = uploadImage.snapshot.downloadUrl
+     })
+     setVisible(false)
+ };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Search Gala </h1>
         <div className="options container-fluid">
-          <Button style={{margin: "15px"}} compact inverted color='orange'> Upload a new file </Button>
+          <Button 
+          style={{margin: "15px"}} 
+          onClick={()=> setVisible(true)}
+          compact 
+          inverted 
+          color='orange'> 
+          Upload a new file 
+          </Button>
           <Button style={{margin: "15px"}} compact inverted color='orange'> Filter </Button>
           <Button style={{margin: "15px"}} compact inverted color='orange'> Voice Typing </Button>
         </div>
@@ -17,6 +45,37 @@ function App() {
           <input type="text" id="box" placeholder="Search anything..." class="search__box" />
           <i class="fas fa-search search__icon" id="icon"></i>
         </div>
+        <Modal
+        basic
+        onClose={() => setVisible(false)}
+        onOpen={() => setVisible(true)}
+        open={visible}
+        size='small'
+        >
+      <Header icon>
+        <Icon name='upload' />
+        Upload File
+      </Header>
+      <Modal.Content>
+        <input type="file" onChange={fileEventHandler} />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button 
+        basic 
+        color='red' 
+        inverted 
+        onClick={() => setVisible(false)}>
+          <Icon name='remove' /> Cancel
+        </Button>
+        <Button 
+        color='green'
+        type="submit" 
+        inverted 
+        onClick={fileUploadHandler}>
+          <Icon name='checkmark' /> Upload
+        </Button>
+      </Modal.Actions>
+    </Modal>
       </header>
     </div>
   );
--- src/upload.js
+++ src/upload.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import firebase from './firebase'
+import { Button} from 'semantic-ui-react'
 
-export default function Upload() {
+export default function Upload(props) {
 
   const [ selectedFile, setSelectedFile ] = useState(null);
+  const [visible, setVisible] = useState(props.visible);
 
   const fileEventHandler = (event) => {
      setSelectedFile(event.target.files[0])
@@ -17,15 +19,41 @@ export default function Upload() {
       () => {
         let downloadUrl = uploadFile.snapshot.downloadUrl
       })
+    setVisible(false);
   };
 
   return (
-    <div className="App">
-      <input type="file" onChange={fileEventHandler} />
-      <button type="submit" onClick={fileUploadHandler}>
-        {" "}
-        Upload{" "}
-      </button>
+    <div style={{alignContent: "center"}} >
+      <Modal
+        basic
+        onClose={() => setVisible(false)}
+        onOpen={() => setVisible(true)}
+        open={visible}
+        size='small'
+        >
+      <Header icon>
+        <Icon name='upload' />
+        Upload File
+      </Header>
+      <Modal.Content>
+        <input type="file" onChange={fileEventHandler} />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button 
+        basic 
+        color='red' 
+        inverted 
+        onClick={() => setVisible(false)}>
+          <Icon name='remove' /> Cancel
+        </Button>
+        <Button 
+        color='green' 
+        inverted 
+        onClick={fileUploadHandler}>
+          <Icon name='checkmark' /> Upload
+        </Button>
+      </Modal.Actions>
+    </Modal>
     </div>
   );
 }
