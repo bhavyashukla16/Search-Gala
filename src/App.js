858a90d3d6d075d03b2a81574d8b1b638d33282f
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-05-08 15:13:58 +0530

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import './App.css';
-//import Upload from './upload';
+import Upload from './components/upload';
 import 'semantic-ui-css/semantic.min.css'
-import firebase from './firebase'
-import { Button, Modal, Header, Icon  } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import { useState } from 'react';
 
 function App() {
 
-  const [ selectedFile, setSelectedFile ] = useState(null);
   const [visible, setVisible] = useState(false);
-
-  const fileEventHandler = (event) => {
-    setSelectedFile(event.target.files[0])
-    };
-
- const fileUploadHandler = () => {
-   let bucketName = 'files'
-   let storageRef = firebase.storage().ref(`${bucketName}/${selectedFile.name}`)
-   let uploadImage = storageRef.put(selectedFile)
-   uploadImage.on(firebase.storage.TaskEvent.STATE_CHANGED,
-     () => {
-       let downloadUrl = uploadImage.snapshot.downloadUrl
-     })
-     setVisible(false)
- };
-
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -45,37 +28,7 @@ function App() {
           <input type="text" id="box" placeholder="Search anything..." class="search__box" />
           <i class="fas fa-search search__icon" id="icon"></i>
         </div>
-        <Modal
-        basic
-        onClose={() => setVisible(false)}
-        onOpen={() => setVisible(true)}
-        open={visible}
-        size='small'
-        >
-      <Header icon>
-        <Icon name='upload' />
-        Upload File
-      </Header>
-      <Modal.Content>
-        <input type="file" onChange={fileEventHandler} />
-      </Modal.Content>
-      <Modal.Actions>
-        <Button 
-        basic 
-        color='red' 
-        inverted 
-        onClick={() => setVisible(false)}>
-          <Icon name='remove' /> Cancel
-        </Button>
-        <Button 
-        color='green'
-        type="submit" 
-        inverted 
-        onClick={fileUploadHandler}>
-          <Icon name='checkmark' /> Upload
-        </Button>
-      </Modal.Actions>
-    </Modal>
+        { visible && <Upload visible={visible} changeVisible={() => setVisible(false)}/> }
       </header>
     </div>
   );
